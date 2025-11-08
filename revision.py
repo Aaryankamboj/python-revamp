365d3a58e111f8d56f3227bb9ada99ff1278512e
Aaryan <a.k.65983.586@gmail.com>
2025-11-08 13:59:05 +0530

    Starting the revamp practice of code

---
+++ revision.py
@@ -0,0 +1,19 @@
+'''print("I am back")
+a=1
+b=0
+if (a>b):
+    print(f"{a} is greater than {b}")
+
+'''
+
+#x = complex(4j)
+x = list((5,3,6))
+print(x)
+print(type(x))
+
+fl = 83.7e3
+print(fl)
+
+bd = 10
+tmp = float(bd)
+print(tmp)
