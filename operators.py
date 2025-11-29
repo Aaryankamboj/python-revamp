4879aef6fe565236b8aec70e5899f0f2249f8d90
Aaryan <a.k.65983.586@gmail.com>
2025-11-29 18:17:31 +0530

    Final commit of the day

---
+++ operators.py
@@ -0,0 +1,28 @@
+num1 =49
+num2 = 7
+print(num1//num2)
+print(type(num1//num2))
+print(num2**2)
+num1+=45
+print(num1)
+
+num2=55
+print(num2)
+x = 4
+print(x&2)
+
+name = "aaryan"
+n=len(name)
+
+if((length:=n))>10:
+    print(n)
+
+
+print(bool(type(10)==type(10.0)))
+
+n=90
+print(1<n>100)
+print(89 <n and 91 > n)
+
+answer = True and False
+print(answer)
